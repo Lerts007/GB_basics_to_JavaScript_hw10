37f16bc87a726bbd20a71904273a02d943983a66
Lerts <oddff@mail.ru>
2023-09-12 21:14:42 +0300

    data.js and img

---
+++ data.js
@@ -0,0 +1,82 @@
+export const yachts = `[
+  {
+    "id": "1",
+    "img": "./img/Yacht1.jpg",
+    "yachtName": "Azimut Atlantis 51",
+    "power": "600",
+    "maxSpeed": "35.0",
+    "numberOfBeds": "6"
+  },
+  {
+    "id": "2",
+    "img": "./img/Yacht2.jpg",
+    "yachtName": "Princess V40",
+    "power": "600",
+    "maxSpeed": "38.0",
+    "numberOfBeds": "4"
+  },
+  {
+    "id": "3",
+    "img": "./img/Yacht3.jpg",
+    "yachtName": "Galeon 460 Fly",
+    "power": "600",
+    "maxSpeed": "30.0",
+    "numberOfBeds": "6"
+  },
+  {
+    "id": "4",
+    "img": "./img/Yacht4.jpg",
+    "yachtName": "Lagoon 400 S2",
+    "power": "400",
+    "maxSpeed": "40.0",
+    "numberOfBeds": "8"
+  },
+  {
+    "id": "5",
+    "img": "./img/Yacht5.jpg",
+    "yachtName": "Azimut 50",
+    "power": "670",
+    "maxSpeed": "32.0",
+    "numberOfBeds": "6"
+  },
+  {
+    "id": "6",
+    "img": "./img/Yacht6.jpg",
+    "yachtName": "Cranchi E 52 F Evoluzione",
+    "power": "600",
+    "maxSpeed": "31.0",
+    "numberOfBeds": "6"
+  },
+  {
+    "id": "7",
+    "img": "./img/Yacht7.jpg",
+    "yachtName": "Azimut Grande 30 METRI",
+    "power": "2400",
+    "maxSpeed": "29.0",
+    "numberOfBeds": "10"
+  },
+  {
+    "id": "8",
+    "img": "./img/Yacht8.jpg",
+    "yachtName": "Pershing 8X",
+    "power": "2638",
+    "maxSpeed": "45.0",
+    "numberOfBeds": "8"
+  },
+  {
+    "id": "9",
+    "img": "./img/Yacht9.jpg",
+    "yachtName": "Princess S80",
+    "power": "2000",
+    "maxSpeed": "33.0",
+    "numberOfBeds": "8"
+  },
+  {
+    "id": "10",
+    "img": "./img/Yacht10.jpg",
+    "yachtName": "Pershing 9X",
+    "power": "2638",
+    "maxSpeed": "42.0",
+    "numberOfBeds": "12"
+  }
+]`;
